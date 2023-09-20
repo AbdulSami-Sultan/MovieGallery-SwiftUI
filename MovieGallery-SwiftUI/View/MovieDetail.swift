bd9d07d6476a63f0894c1a5bf4021b4bfdc1b5dc
Abdul Sami Sultan <56970013+AbdulSami-Sultan@users.noreply.github.com>
2023-09-20 23:46:28 +0500

    home screen with featured added

--- MovieGallery-SwiftUI/View/MovieDetail.swift
+++ MovieGallery-SwiftUI/View/MovieDetail.swift
@@ -8,13 +8,48 @@
 import SwiftUI
 
 struct MovieDetail: View {
+    var movie: Movie
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 10) {
+            ZStack(alignment: .bottom) {
+                Image(movie.thumbnail)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Rectangle()
+                    .frame(height: 80)
+                    .opacity(0.25)
+                    .blur(radius: 10)
+                HStack {
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text(movie.title)
+                            .foregroundColor(.white)
+                            .bold()
+                            .font(.largeTitle)
+                    }
+                    .padding(.leading)
+                    .padding(.bottom)
+                    Spacer()
+                }
+            }
+            VStack(alignment: .center,spacing: 15) {
+                Text(movie.description)
+                    .foregroundColor(.primary)
+                    .font(.body)
+                    .lineSpacing(14)
+                
+//                WatchButton(movie: movie)
+                    .padding()
+            }.padding(.all)
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0,maxHeight: .infinity, alignment: .topLeading)
+        }
+        .edgesIgnoringSafeArea(.top)
+        .navigationBarHidden(true)
     }
 }
 
 struct MovieDetail_Previews: PreviewProvider {
     static var previews: some View {
-        MovieDetail()
+        MovieDetail(movie: moviesData.first!)
     }
 }
---
+++ MovieGallery-SwiftUI/View/PageView.swift
@@ -0,0 +1,27 @@
+//
+//  PageView.swift
+//  MovieGallery-SwiftUI
+//
+//  Created by Abdul Sami Sultan on 20/09/2023.
+//
+
+import SwiftUI
+
+import SwiftUI
+
+struct PageView<Page: View>: View {
+    var viewControllers: [UIHostingController<Page>]
+    @State var currentPage = 0
+    
+    init(_ views: [Page]) {
+        self.viewControllers = views.map { UIHostingController(rootView: $0) }
+    }
+    
+    var body: some View {
+        ZStack(alignment: .bottomTrailing) {
+            PageViewController(controllers: viewControllers, currentPage: $currentPage)
+            PageControl(numberOfPages: viewControllers.count, currentPage: $currentPage)
+                .padding(.trailing)
+        }
+    }
+}
