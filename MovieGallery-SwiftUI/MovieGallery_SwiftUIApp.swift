d857f42604b8fd77e06931de8d446c34904fb8b5
Abdul Sami Sultan <56970013+AbdulSami-Sultan@users.noreply.github.com>
2023-09-20 23:08:29 +0500

    Initial Commit

---
+++ MovieGallery-SwiftUI/MovieGallery_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieGallery_SwiftUIApp.swift
+//  MovieGallery-SwiftUI
+//
+//  Created by Abdul Sami Sultan on 20/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieGallery_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
